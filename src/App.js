0133a68fe15c1cbd6fc55ed8c90fe115fe1eec35
DannyGalvan <cgalvan29111999@gmail.com>
2023-04-12 00:48:35 -0600

    Ejercicios_sesion_1_2_3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Clock from "./components/pure/Clock";
-import ClockFunctional from "./components/pure/ClockFunctional";
+import { ContactList } from "./components/containers/ContactList";
 
 function App() {
-
   return (
     <div className="App">
       <main className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Clock />
-        <ClockFunctional />
+        <ContactList />
       </main>
     </div>
   );
